08f1d3c7b3d2f95be93dc218e4f6451995fd7088
rushikesh veeramally <43981800+rushikeshveeramally@users.noreply.github.com>
2023-06-14 11:06:58 -0500

    added thread assignments

---
+++ com/Threads/Thread1.java
@@ -0,0 +1,15 @@
+package com.Threads;
+
+public class Thread1 extends Thread{
+    public void run(){
+        for (int i=0;i<=10;i++){
+            System.out.println(i);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Thread1 thread1 =new Thread1();
+        thread1.start();
+    }
+}
---
+++ com/Threads/ThreadAssignment2.java
@@ -0,0 +1,16 @@
+package com.Threads;
+
+public class ThreadAssignment2 implements Runnable{
+    @Override
+    public void run() {
+        for (int i=0;i<=10;i++){
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        ThreadAssignment2 thread2 =new ThreadAssignment2();
+        Thread thread = new Thread(thread2);
+        thread.start();
+    }
+}
