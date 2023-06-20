59fcbc6e7a98ca32320895f2af69acdd29799823
rushikesh <43981800+rushikeshveeramally@users.noreply.github.com>
2023-06-20 14:18:07 -0500

    added 4 files

---
+++ com/collectionsA/ArrayListTraversalExample.java
@@ -0,0 +1,71 @@
+package com.collectionsA;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class ArrayListTraversalExample {
+    public static void main(String[] args) {
+        List<String> stringList = new ArrayList<>();
+        stringList.add("java");
+        stringList.add("full");
+        stringList.add("stack");
+        stringList.add("Developer");
+
+        // Using for loop
+        System.out.println("Using for loop:");
+        for (int i = 0; i < stringList.size(); i++) {
+            String element = stringList.get(i);
+            System.out.println(element);
+        }
+
+        // Using enhanced for loop
+        System.out.println("\nUsing enhanced for loop:");
+        for (String element : stringList) {
+            System.out.println(element);
+        }
+
+        // Using iterator
+        System.out.println("\nUsing iterator:");
+        Iterator<String> iterator = stringList.iterator();
+        while (iterator.hasNext()) {
+            String element = iterator.next();
+            System.out.println(element);
+        }
+
+        // Using forEach
+        System.out.println("\nUsing forEach:");
+        stringList.forEach(System.out::println);
+
+        // Using parallel stream
+        System.out.println("\nUsing parallel stream:");
+        stringList.parallelStream().forEach(System.out::println);
+
+
+        //After removal
+
+
+
+        System.out.println("After removal");
+
+        System.out.println( stringList.remove(stringList.size()-1) + "is removed");
+
+        System.out.println("array list after removing last element is"+stringList);
+
+
+        //converting arraylist to array
+
+        System.out.println("converting arraylist to array");
+
+        String[] stArray = stringList.toArray(new String[0]);
+
+        for (String e : stArray){
+            System.out.println(e);
+        }
+
+        System.out.println(stArray);
+
+
+
+    }
+}
---
+++ com/collectionsA/RemoveStudentsExample.java
@@ -0,0 +1,51 @@
+package com.collectionsA;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Student {
+    private String firstName;
+    private String lastName;
+    private double gpa;
+
+    public Student(String firstName, String lastName, double gpa) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gpa = gpa;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName + ", GPA: " + gpa;
+    }
+}
+
+public class RemoveStudentsExample {
+    public static void main(String[] args) {
+        List<Student> studentList = new ArrayList<>();
+        studentList.add(new Student("rushikesh", "v", 3.5));
+        studentList.add(new Student("java", "developer", 4.0));
+        studentList.add(new Student("spring", "Boot", 2.8));
+        studentList.add(new Student("UI", "Angular", 3.9));
+
+        // Calculate average GPA
+        double totalGpa = 0;
+        for (Student student : studentList) {
+            totalGpa += student.getGpa();
+        }
+        double averageGpa = totalGpa / studentList.size();
+
+        // Remove students with GPA less than average
+        studentList.removeIf(student -> student.getGpa() < averageGpa);
+
+        // Print remaining students
+        System.out.println("Remaining students:");
+        for (Student student : studentList) {
+            System.out.println(student);
+        }
+    }
+}
